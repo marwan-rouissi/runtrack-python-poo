61b787737822459277d79d0e067203b4fe6dda4a
marwan-rouissi <115158061+marwan-rouissi@users.noreply.github.com>
2023-03-02 08:48:06 +0100

    MAJ job03

--- jour03/job03.py
+++ jour03/job03.py
@@ -23,63 +23,77 @@ class ListeDeTaches:
 
     # méthode pour ajouter une tache à la liste
     def ajouterTache(self, tache):
-        tache = [tache.titre, tache.description, tache.statut]
+        tache = {"Titre":tache.titre, "Description": tache.description,"Statut": tache.statut}
         self.taches.append(tache)
 
     # méthode pour supprimer une tache de la liste
     def supprimerTache(self, tache):
-        tache = [tache.titre, tache.description, tache.statut]
+        tache = {"Titre":tache.titre, "Description": tache.description,"Statut": tache.statut}
         self.taches.remove(tache)
 
     # méthode pour changer l'état de la tache
     def marquerCommeFinie(self, tache):
-        tache.description = "terminer"
+        tache = {"Titre":tache.titre, "Description": tache.description,"Statut": tache.statut}
+        for i in self.taches:
+            if tache == i:
+                tache["Statut"] = "terminée"
 
-        tache.setStatut(tache.description)
-        # print(tache.description)
+                i.update(tache)
+
         
 
     # méthode pour afficher la liste de taches
     def afficherListe(self):
 
-        for i in range(len(self.taches)):
-                print(f"Tâche: {self.taches[i][0]}. Description: {self.taches[i][1]}")
+        for tache in self.taches:
+            print("tache:", tache["Titre"], ", description:", tache["Description"], ", statut:", tache["Statut"])
+            # print(f"Tâche: {self.taches [0]["Titre"]}")
 
     # méthode à revoir
     def filtrerListe(self, statut):
-        tache_encours = []
-        tache_faites = []
 
-        
-        for i in range(len(self.taches)):
-            
-                # print(i)
-                print(f"Tâche: {self.taches[i][0]}. Description: {self.taches[i][1]}, état: {self.taches[i][2]}")
-                print('liste en cours', tache_encours)
-                print('liste terminer', tache_faites)
-        
+        for tache in self.taches:
+            if tache["Statut"] == statut:
+                print("tache:", tache["Titre"], ", description:", tache["Description"], ", statut:", tache["Statut"])
+            else:
+                pass
+
 # Instancier des taches
 
 tache_1 = Tache("Réviser", "cours de maths", "à faire")
 tache_2 = Tache("Sport", "Aller à la salle", "à faire")
 tache_3 = Tache("Manger", "Aller manger", "à faire")
 
+# Instancier une liste de tache
+
 listTache = ListeDeTaches()
 
-print("-----------------------------------------------------")
+# Imprimer et vérifier le bon fonctionnement des méthodes
+
+print("-----------------------------------------------------------------")
 print("Ajouter 3 taches")
-print("-----------------------------------------------------")
+print("-----------------------------------------------------------------")
 
 listTache.ajouterTache(tache_1)
 listTache.ajouterTache(tache_2)
 listTache.ajouterTache(tache_3)
 listTache.afficherListe()
-print("-----------------------------------------------------")
+print("-----------------------------------------------------------------")
 print("Supprimer tache 1")
-print("-----------------------------------------------------")
+print("-----------------------------------------------------------------")
 listTache.supprimerTache(tache_1)
-listTache.marquerCommeFinie(tache_3)
-
-# listTache.filtrerListe("en cours")
-
 listTache.afficherListe()
+print("-----------------------------------------------------------------")
+print("Marquer tache 3 comme terminée")
+print("-----------------------------------------------------------------")
+listTache.marquerCommeFinie(tache_3)
+listTache.afficherListe()
+print("-----------------------------------------------------------------")
+print("Filter taches à faire")
+print("-----------------------------------------------------------------")
+listTache.filtrerListe("à faire")
+print("-----------------------------------------------------------------")
+print("Filter taches terminée")
+print("-----------------------------------------------------------------")
+listTache.filtrerListe("terminée")
+print("-----------------------------------------------------------------")
