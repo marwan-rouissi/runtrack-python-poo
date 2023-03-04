8ea1c89c941a17f0f87d7b236484794218765e07
marwan-rouissi <115158061+marwan-rouissi@users.noreply.github.com>
2023-03-04 10:07:27 +0100

    MAJ job05

--- jour05/job05.py
+++ jour05/job05.py
@@ -1,14 +1,18 @@
 # job 5
 
-n = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+n = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+termeSuivant = 10
 
 def fibonacci(n):
     # condition de break
+    # les 2 premiers termes de la suite valent 1 chacun
     if n <= 2:
         return 1
     
     else:
-        
+        # retourner la somme de l'index précédent et de celui qui le précéde
         return (fibonacci(n-1) + fibonacci(n-2))
 
-print(fibonacci(9))
+print(f"Les 9 premiers termes de la suite de Fibonacci sont:\n{n}")
+print(f"Le {termeSuivant}e terme sera: {fibonacci(termeSuivant)}")
